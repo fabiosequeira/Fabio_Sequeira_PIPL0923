61ab2e6ead466c4b0c75efa596281cab605466f5
Fábio Sequeira <fabio.sequeira.t0126448@edu.atec.pt>
2025-02-20 10:38:08 +0000

    test11

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
-printf("Hello World");
+
+int num = 0;
+for(int i = 0; i < 10; i++){
+    num += 2;
+    printf("num = %d, i = %d\n", num, i);
+}
+
+
 return 0;
 }
