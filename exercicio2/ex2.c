227baa4f7d10f32d142b28d0db2cc2530b540dcc
Fábio Sequeira <fabio.sequeira.t0126448@edu.atec.pt>
2025-02-25 15:03:29 +0000

    done

---
+++ exercicio2/ex2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    printf("Digite o terceiro número: ");
+    scanf("%d", &num3);
+
+    int maior = num1;
+    if (num2 > maior) {
+        maior = num2;
+    }
+    if (num3 > maior) {
+        maior = num3;
+    }
+
+    int menor = num1;
+    if (num2 < menor) {
+        menor = num2;
+    }
+    if (num3 < menor) {
+        menor = num3;
+    }
+
+    printf("O maior número é: %d\n", maior);
+    printf("O menor número é: %d\n", menor);
+
+    return 0;
+}
